8599ee3d8f1171d28aadabd8f35ef7a7c28f28ea
Muhammad Taha <mmuhammadtaha786@gmail.com>
2025-08-09 03:41:42 +0500

    First Push from Vs code via Git

---
+++ src/dto/create-user.dto.ts
@@ -0,0 +1,9 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty({ message: 'Name is required' })
+  name: string;
+
+  @IsEmail({}, { message: 'Invalid email format' })
+  email: string;
+}
---
+++ src/user.service.ts
@@ -0,0 +1,39 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UsersService {
+  private users = [{ id: 1, name: 'Muhammad Taha', email: 'taha@taha.com' }];
+
+  findAll() {
+    return this.users;
+  }
+
+  findOne(id: number) {
+    const user = this.users.find((u) => u.id === id);
+    if (!user) throw new NotFoundException('User not found');
+    return user;
+  }
+
+  create(dto: CreateUserDto) {
+    const id = this.users.length + 1;
+    const newUser = { id, ...dto };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  update(id: number, dto: UpdateUserDto) {
+    const index = this.users.findIndex((u) => u.id === id);
+    if (index === -1) throw new NotFoundException('User not found');
+    this.users[index] = { id, ...dto };
+    return this.users[index];
+  }
+
+  delete(id: number) {
+    const index = this.users.findIndex((u) => u.id === id);
+    if (index === -1) throw new NotFoundException('User not found');
+    const deleted = this.users.splice(index, 1);
+    return deleted[0];
+  }
+}
